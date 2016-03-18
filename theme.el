da267593736ef3351bfde444734f68c34582ac5d
digiorgi <hernan.digiorgi@gmail.com>
2016-03-18 14:20:15 -0300

    clj

--- theme.el
+++ theme.el
@@ -1,7 +1,7 @@
-(load-theme 'soothe t) 
+(load-theme 'monokai t) 
 (setq visible-bell 1)
 (global-hl-line-mode)
-(set-face-attribute 'default nil :height 107)
+(set-face-attribute 'default nil :height 110)
 (setq ring-bell-function 'ignore)
 
 ; Remove any bars... only text
